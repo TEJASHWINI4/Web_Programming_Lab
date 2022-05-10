136350db43010b0aaeca04a20fd7bf5d2064c956
TEJASHWINI4 <87752086+TEJASHWINI4@users.noreply.github.com>
2022-05-10 09:04:28 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,108 @@
+/*console.log(1)
+    console.log("hello");
+    arrsort = (arr) => {
+        for (let i = 0; i < arr.length - 1; i++) {
+            for (let j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    let temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+
+    let arr = [4, 6, 7, 8, 2, 1, -2];
+    let res = [];
+    res = arrsort(arr);
+    console.log(arr)*/
+
+/*console.log(2)
+freq = (arr1) => {
+    let total = 0;
+    let ele;
+    for (let i = 0; i < arr1.length; i++) {
+        let count = 1;
+        for (let j = i + 1; j < arr1.length; j++) {
+            if (arr1[i] == arr1[j]) {
+                count = count + 1;
+            }
+        }
+        if (count > total) {
+            total = count;
+            ele = arr1[i];
+        }
+    }
+    return ele;
+}
+let arr1 = [1, 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 'a'];
+let res = freq(arr1);
+console.log(res) */
+/*
+com = (arr1, arr2) => {
+    if (arr1.length == arr2.length) {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return 0;
+            }
+        }
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+let arr1 = [7, 8, 9, 4, 5];
+let arr2 = [7, 8, 9, 4, 5];
+let res = com(arr1, arr2);
+if (res == 1) {
+    console.log("both are identical");
+} else {
+    console.log(" both are un identical");
+} */
+
+/*//Method 1
+let sal = ["kittu", 25000, "surya", 50000, true];
+console.log(sal);*/
+
+/*//Method2
+let num = new Array(10, 20, 30, 40, 50);
+let  num1 = new Array(5);
+let home = new Array("1BHK");
+console.log(num,num1,home);
+num1[0]="mohan";*/
+
+/*//Accessing Array Elements
+let name1 = ["mohan", 25000, "kumar", 50000, true];
+alert(name1[0]+" sal= "+name1[1]);
+let sal2 = name1[3];
+alert("Kumar Salary= "+sal2);*/
+
+/*//Length Property of an Array
+let name1 = ["mohan", 25000, "kumar", 50000, true]; 
+let len = name1.length;
+for(let i=0;i<len;i++)
+alert(name1[i]);*/
+/*
+let va = ("kittu")
+let va = ("lalit")
+console.log(va)
+*/
+/*
+var va1 = ("kittu")
+var va1 = ("lalit")
+console.log(va1)*/
+/*
+const va2 = ("kittu")
+const va2 = ("lalit")
+console.log(va2)*/
+
+duplicate = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.lenght - 1; j++) {
+
+        }
+    }
+}
