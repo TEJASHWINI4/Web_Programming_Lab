e047ea783d9145706c21f1ed0b7535a2a59fa61e
TEJASHWINI4 <87752086+TEJASHWINI4@users.noreply.github.com>
2022-05-31 06:30:49 +0530

    Delete map.js

--- map.js
+++
@@ -1,8 +0,0 @@
-let n = prompt("enter the lenght of array");
-
-let arr = [];
-for (let i = 0; i < n; i++) {
-    arr[i] = parseInt(prompt("enter the elements"));
-}
-let res = arr.map((x) => x * 10);
-console.log(res);
